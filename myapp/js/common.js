fb3cd2625551ce0624530439d460bd07cd621198
Lia <liaforweb@gmail.com>
2020-05-08 07:54:15 +0300

    home

--- myapp/js/common.js
+++ myapp/js/common.js
@@ -8,145 +8,51 @@ $(function() {
             return false;
         });
     }
-	// Custom JS
-    $('.stations__slider').on('initialized.owl.carousel changed.owl.carousel', function (e) {
-        if (!e.namespace) {
-            return;
-        }
-        let carousel = e.relatedTarget;
-        $('.slider-counter').text(carousel.relative(carousel.current()) + 1 + '/' + carousel.items().length);
-        $('.stations__item').removeClass('active');
-    }).owlCarousel({
-        loop: false,
-        dots: false,
-        margin: 10,
-        nav: true,
-        navText: ['<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve"><metadata> Svg Vector Icons : http://www.onlinewebfonts.com/icon </metadata><g><g transform="matrix(1 0 0 -1 0 1008)"><path d="M756.2,741.8L990,508L756.2,274.2l-27,27L918.1,490H10v36h908.1L729.3,714.8L756.2,741.8z"/></g></g></svg>', '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve"><metadata> Svg Vector Icons : http://www.onlinewebfonts.com/icon </metadata><g><g transform="matrix(1 0 0 -1 0 1008)"><path d="M756.2,741.8L990,508L756.2,274.2l-27,27L918.1,490H10v36h908.1L729.3,714.8L756.2,741.8z"/></g></g></svg>'],
-        responsive: {
-            0: {
-                items: 1
-            },
-            768: {
-                items: 1
-            },
-            1000: {
-                items: 1
-            }
-        }
-    });
-});
 
-
-
-document.addEventListener('DOMContentLoaded', function(){
-//menu
-    var menu = document.querySelector('.menu-toggle');
-    menu.addEventListener('click', function(){
-        var nav = document.querySelector('.main-menu');
-        nav.classList.toggle('active');
-        var navGamb = document.querySelector('.menu-toggle');
-        navGamb.classList.toggle('active');
-    });
-//tabs
-	// store tabs variable
-	var myTabs = document.querySelectorAll("ul.header__tabs > li");
-    function myTabClicks(tabClickEvent) {
-		for (var i = 0; i < myTabs.length; i++) {
-			myTabs[i].classList.remove("active");
-		}
-		var clickedTab = tabClickEvent.currentTarget;
-		clickedTab.classList.add("active");
-		tabClickEvent.preventDefault();
-		var myContentPanes = document.querySelectorAll(".tab-pane");
-		for (i = 0; i < myContentPanes.length; i++) {
-			myContentPanes[i].classList.remove("active");
-		}
-        var anchorReference = tabClickEvent.target;
-        var activePaneId = anchorReference.getAttribute("href");
-        var activePane = document.querySelector(activePaneId);
-		activePane.classList.add("active");
-    }
-    for (i = 0; i < myTabs.length; i++) {
-		myTabs[i].addEventListener("click", myTabClicks)
-	}
-
-
-
-
-
-});
-
-$(function() {
     //select-number form
     if(jQuery('.phone-mask').length) {
         jQuery(function($){
             $(".phone-mask").mask("+38(999) 999-9999");
         });
     }
-    //time
-    if (window.innerHeight < 821 || window.screen.height < 821) {
-        $('.time__num').on('click', function(){
-            $(this).parent().siblings().children().removeClass('active');
-            $(this).next().toggleClass('active');
-        });
-        $(document).mouseup(function (e){ // событие клика по веб-документу
-            var div = $(".time__num"); // тут указываем ID элемента
-            if (!div.is(e.target) // если клик был не по нашему блоку
-            && div.has(e.target).length === 0) { // и не по его дочерним элементам
-                $('.time__num').parent().siblings().children().removeClass('active');
-            }
-        });
-    }
-    // $('.stations__slider').on('initialized.owl.carousel changed.owl.carousel', function (e) {
-    //     if (!e.namespace) {
-    //         return;
-    //     }
-    //     let carousel = e.relatedTarget;
-    //     $('.slider-counter').text(carousel.relative(carousel.current()) + 1 + '/' + carousel.items().length);
-    //     $('.stations__item').removeClass('active');
-    // }).owlCarousel({
-    //     loop: false,
-    //     dots: false,
-    //     margin: 10,
-    //     nav: true,
-    //     navText: ['<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve"><metadata> Svg Vector Icons : http://www.onlinewebfonts.com/icon </metadata><g><g transform="matrix(1 0 0 -1 0 1008)"><path d="M756.2,741.8L990,508L756.2,274.2l-27,27L918.1,490H10v36h908.1L729.3,714.8L756.2,741.8z"/></g></g></svg>', '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve"><metadata> Svg Vector Icons : http://www.onlinewebfonts.com/icon </metadata><g><g transform="matrix(1 0 0 -1 0 1008)"><path d="M756.2,741.8L990,508L756.2,274.2l-27,27L918.1,490H10v36h908.1L729.3,714.8L756.2,741.8z"/></g></g></svg>'],
-    //     responsive: {
-    //         0: {
-    //             items: 1
-    //         },
-    //         768: {
-    //             items: 1
-    //         },
-    //         1000: {
-    //             items: 1
-    //         }
-    //     }
-    // });
-    // $('.reviews__slider').on('initialized.owl.carousel changed.owl.carousel', function (e) {
-    //     if (!e.namespace) {
-    //         return;
-    //     }
-    //     let carousel = e.relatedTarget;
-    //     $('.slider-counter--reviews').text(carousel.relative(carousel.current()) + 1 + '/' + carousel.items().length);
-    // }).owlCarousel({
-    //     loop: false,
-    //     dots: false,
-    //     margin: 10,
-    //     nav: true,
-    //     navText: ['<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve"><metadata> Svg Vector Icons : http://www.onlinewebfonts.com/icon </metadata><g><g transform="matrix(1 0 0 -1 0 1008)"><path d="M756.2,741.8L990,508L756.2,274.2l-27,27L918.1,490H10v36h908.1L729.3,714.8L756.2,741.8z"/></g></g></svg>', '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve"><metadata> Svg Vector Icons : http://www.onlinewebfonts.com/icon </metadata><g><g transform="matrix(1 0 0 -1 0 1008)"><path d="M756.2,741.8L990,508L756.2,274.2l-27,27L918.1,490H10v36h908.1L729.3,714.8L756.2,741.8z"/></g></g></svg>'],
-    //     responsive: {
-    //         0: {
-    //             items: 1
-    //         },
-    //         768: {
-    //             items: 1
-    //         },
-    //         1000: {
-    //             items: 1
-    //         }
-    //     }
-    // });
 
+    //tabs
+    $('.tabs__caption').on('click', 'li:not(.active)', function() {
+        $(this)
+          .addClass('active').siblings().removeClass('active')
+          .closest('.tabs').find('.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+      });
+    if(jQuery('.gallery__body').length) {
+        $('.gallery__body').slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            centerMode: true,
+            variableWidth: true,
+            arrows: false,
+            dots: true,
+            responsive: [
+                {
+                  breakpoint: 768,
+                  settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                  }
+                },
+                {
+                  breakpoint: 480,
+                  settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                  }
+                }
+              ]
+          });
+    }
     //start note slider
     if(jQuery('.rev_slider').length) {
         var rev = $('.rev_slider');
@@ -262,29 +168,6 @@ $(function() {
       }
     });
 }
-
-    //kviz
-    if(jQuery('.kviz').length) {
-        $('.qa-next').click(function(e){
-            e.preventDefault();
-            if($(this).parent().prev().find('input:checked').length) {
-                $(this).parent().parent('.step-slide').removeClass('step-slide--active').next().addClass('step-slide--active');
-            } else {
-                $(this).parent().find('.kviz__error').text('Выберите вариант ответа!');
-            }
-
-            
-        });
-        // for radiobuttons
-        $('input[type="radio"]+.pick-item__label').click(function(e){
-            $(this).parent().parent().parent('.step-slide').removeClass('step-slide--active').next().addClass('step-slide--active');           
-        });
-
-        $(".qa-prev").click(function(e) {
-            e.preventDefault();
-            $(this).parent().parent('.step-slide').removeClass('step-slide--active').prev().addClass('step-slide--active');
-        });
-    }
     // UTM
     function getQueryVariable(variable) {
         var query = window.location.search.substring(1);
